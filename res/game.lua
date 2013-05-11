8e1adc53a7cb9bd714db32edc7761003e2912ec4
Marc Lepage <mlepage@antimeta.com>
2013-05-11 14:11:59 -0400

    Change to orthographic projection, add a few tiles, and sample form UI.

--- res/game.lua
+++ res/game.lua
@@ -1,67 +1,56 @@
--- This lua script file represents a lua implementation translation of sample00-mesh with a box instead of a duck.
+-- Memory game
+-- Copyright (C) 2013 Marc Lepage
 
-function initialize()
-    -- Display splash screen for at least 1 second.
-    ScreenDisplayer.start("drawSplash", 1000)
+local game
 
-    _touched = false
-    _touchX = 0
+local scene
 
-    -- Load font
-    --_font = Font.create("res/arial40.gpb")
-
-    -- Load mesh/scene from file
-    _scene = Scene.load("res/box.gpb")
-
-    -- Get the box node
-    _modelNode = _scene:findNode("box")
-
-    -- Bind the material to the model
-    _modelNode:getModel():setMaterial("res/box.material")
-
-    -- Find the light node
-    local lightNode = _scene:findNode("directionalLight1")
-
-    -- Bind the light node's direction into the box material.
-    _modelNode:getModel():getMaterial():getParameter("u_lightDirection"):bindValue(lightNode, "&Node::getForwardVectorView")
-
-    -- Update the aspect ratio for our scene's camera to match the current device resolution
-    local game = Game.getInstance()
-    _scene:getActiveCamera():setAspectRatio(game:getWidth() / game:getHeight())
-
-    -- Create the grid and add it to the scene.
-    local model = createGridModel()
-    _scene:addNode("grid"):setModel(model)
-
-    -- Load the AI script
-    --game:getScriptController():loadScript("res/ai.lua")
-
-    ScreenDisplayer.finish()
-end
-
-function update(elapsedTime)
-end
+local memory =
+{
+    difficulty = 'normal',
+    sizes = { easy={4,3}, normal={6,4}, hard={8,5} },
+    width = 6,
+    height = 4,
+    tiles = {} -- row major
+}
 
 -- Avoid allocating new objects every frame.
-textColor = Vector4.new(0, 0.5, 1, 1)
+local textColor = Vector4.new(0, 0.5, 1, 1)
 
-function render(elapsedTime)
-    -- Clear the color and depth buffers.
-    Game.getInstance():clear(Game.CLEAR_COLOR_DEPTH, Vector4.zero(), 1.0, 0)
-
-    -- Visit all the nodes in the scene, drawing the models/mesh.
-    _scene:visit("drawScene")
-
-    -- Draw the fps.
-    --local buffer = string.format("%u\n%s", Game.getInstance():getFrameRate(), _stateMachine:getActiveState():getId())
-    --_font:start()
-    --_font:drawText(buffer, 5, 1, textColor, _font:getSize())
-    --_font:finish()
+local function newGame()
+    memory.tiles = {}
+    for r = 1, memory.height do
+        memory.tiles[r] = {}
+        for c = 1, memory.width do
+            local tile = { r=r, c=c }
+            memory.tiles[r][c] = tile
+        end
+    end
 end
 
-function finalize()
-    --_font = nil
-    _scene = nil
+local function createTileNode(x, y, w, h)
+    local mesh = Mesh.createQuad(
+        Vector3.new(-1, -1, 0),
+        Vector3.new(-1, 1, 0),
+        Vector3.new(1, -1, 0),
+        Vector3.new(1, 1, 0))
+    local model = Model.create(mesh)
+    model:setMaterial('res/tile.material')
+    local node = scene:addNode('tile')
+    node:setModel(model)
+    return node
+end
+
+local function createGameView()
+    for r = 1, memory.height do
+        for c = 1, memory.width do
+            local tile = memory.tiles[r][c]
+            local node = createTileNode()
+            node:scale(50, 50, 1)
+            node:translate(c*150, r*150, 0)
+            tile.node = node
+        end
+    end    
 end
 
 function drawScene(node)
@@ -75,10 +64,26 @@ end
 function drawSplash()
     local game = Game.getInstance()
     game:clear(Game.CLEAR_COLOR_DEPTH, 0, 0, 0, 1, 1.0, 0)
-    local batch = SpriteBatch.create("res/logo_powered_white.png")
+    local batch = SpriteBatch.create('res/logo_powered_white.png')
     batch:start()
     batch:draw(game:getWidth() * 0.5, game:getHeight() * 0.5, 0.0, 512.0, 512.0, 0.0, 1.0, 1.0, 0.0, Vector4.one(), true)
     batch:finish()
+end
+
+function _controlEvent(control, event)
+    print('_controlEvent', event)
+    if (event == Control.Listener.CLICK) then
+        local deltaX = 10
+        _modelNode:rotateY(math.rad(deltaX * 0.5))
+    end
+end
+
+function _controlEvent2(control, event)
+    print('_controlEvent2', event)
+    if (event == Control.Listener.CLICK) then
+        local deltaX = -10
+        _modelNode:rotateY(math.rad(deltaX * 0.5))
+    end
 end
 
 function keyEvent(evt, key)
@@ -109,76 +114,72 @@ function touchEvent(evt, x, y, contactIndex)
     end    
 end
 
-function createGridModel()
-    local lineCount = 41
-    local pointCount = lineCount * 4
-    local verticesSize = pointCount * (3 + 3)
+function update(elapsedTime)
+    _form:update(elapsedTime)
+end
 
-    local vertices = {}
-    local gridLength = math.floor(lineCount / 2)
-    local value = -gridLength
+function render(elapsedTime)
+    -- Clear the color and depth buffers.
+    Game.getInstance():clear(Game.CLEAR_COLOR_DEPTH, Vector4.zero(), 1.0, 0)
 
-    while #vertices + 1 < verticesSize do
-        -- Default line color is dark grey
-        local red, green, blue = 0.3, 0.3, 0.3
+    -- Visit all the nodes in the scene, drawing the models/mesh.
+    scene:visit('drawScene')
 
-        -- Every 10th line is brighter grey
-        if math.floor(value + 0.5) % 10 == 0 then
-            red, green, blue = 0.45, 0.45, 0.45
-        end
-        -- The Z axis is blue
-        if value == 0 then
-            red, green, blue = 0.15, 0.15, 0.7
-        end
+    _form:draw()
 
-        -- Build the lines
-        vertices[#vertices+1] = value
-        vertices[#vertices+1] = 0.0
-        vertices[#vertices+1] = -gridLength
-        vertices[#vertices+1] = red
-        vertices[#vertices+1] = green
-        vertices[#vertices+1] = blue
+    -- Draw the fps.
+    --local buffer = string.format('%u\n%s', Game.getInstance():getFrameRate(), _stateMachine:getActiveState():getId())
+    --_font:start()
+    --_font:drawText(buffer, 5, 1, textColor, _font:getSize())
+    --_font:finish()
+end
 
-        vertices[#vertices+1] = value
-        vertices[#vertices+1] = 0.0
-        vertices[#vertices+1] = gridLength
-        vertices[#vertices+1] = red
-        vertices[#vertices+1] = green
-        vertices[#vertices+1] = blue
+function initialize()
+    -- Display splash screen for at least 1 second.
+    --ScreenDisplayer.start('drawSplash', 1000)
 
-        -- The X axis is red
-        if value == 0.0 then
-            red, green, blue = 0.7, 0.15, 0.15
-        end
-        vertices[#vertices+1] = -gridLength
-        vertices[#vertices+1] = 0.0
-        vertices[#vertices+1] = value
-        vertices[#vertices+1] = red
-        vertices[#vertices+1] = green
-        vertices[#vertices+1] = blue
+    game = Game.getInstance()
 
-        vertices[#vertices+1] = gridLength
-        vertices[#vertices+1] = 0.0
-        vertices[#vertices+1] = value
-        vertices[#vertices+1] = red
-        vertices[#vertices+1] = green
-        vertices[#vertices+1] = blue
+    scene = Scene.create()
 
-        value = value + 1.0
-    end
+    _touched = false
+    _touchX = 0
 
-    local elements = { 
-        VertexFormat.Element.new(VertexFormat.POSITION, 3),
-        VertexFormat.Element.new(VertexFormat.COLOR, 3)
-    }
-    local mesh = Mesh.createMesh(VertexFormat.new(elements, 2), pointCount, false)
-    if mesh == nil then
-        return nil, "Error creating grid mesh."
-    end
-    mesh:setPrimitiveType(Mesh.LINES)
-    mesh:setVertexData(vertices, 0, pointCount)
+    -- Load font
+    --_font = Font.create('res/arial40.gpb')
 
-    local model = Model.create(mesh)
-    model:setMaterial("res/grid.material")
-    return model
+    --local camera = Camera.createOrthographic(10, 10, 1280/720, 1, 10)
+    local camera = Camera.createOrthographic(1, 1, 1, 0, 1)
+
+    local matrix = Matrix.new()
+    Matrix.createOrthographicOffCenter(0, game:getWidth(), game:getHeight(), 0, -100, 100, matrix)
+    camera:resetProjectionMatrix()
+    camera:setProjectionMatrix(matrix)
+
+    local cameraNode = scene:addNode('camera')
+    cameraNode:setCamera(camera)
+    scene:setActiveCamera(camera)
+    cameraNode:translate(0, 0, 5);
+
+    newGame()
+    createGameView()
+    _modelNode = memory.tiles[1][1].node
+
+    -- Load the AI script
+    --game:getScriptController():loadScript('res/ai.lua')
+    
+    _form = Form.create('res/editor.form')
+    
+    _reset = _form:getControl('reset');
+    _reset:addScriptCallback('controlEvent', '_controlEvent');
+    _emit = _form:getControl('emit');
+    _emit:addScriptCallback('controlEvent', '_controlEvent2');
+
+    ScreenDisplayer.finish()
+end
+
+function finalize()
+    --_font = nil
+    game = nil
+    scene = nil
 end
--- src/MemoryGame.cpp
+++ src/MemoryGame.cpp
@@ -1,3 +1,18 @@
-#include "MemoryGame.h"
+#include "gameplay.h"
+
+using namespace gameplay;
+
+class MemoryGame: public Game
+{
+protected:
+    
+    void initialize() {}
+    
+    void finalize() {}
+    
+    void update(float elapsedTime) {}
+    
+    void render(float elapsedTime) {}
+};
 
 MemoryGame game;
